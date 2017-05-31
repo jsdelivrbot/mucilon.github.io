8ce062f8c1d0d0a6bbd3339afa42a7666ee9653b
João Graça Neto <joao.g.n91@gmail.com>
2017-05-30 21:54:16 -0300

    teste

--- PhysicsLab/Holograns/Law Kleper/js/TesteBasicSync.js
+++ PhysicsLab/Holograns/Law Kleper/js/TesteBasicSync.js
@@ -11,13 +11,21 @@ initSystem('start()');
 function start(){
 
 
-globalEarth = new Planets('Earth',0.976,1.010,0.0000425,0.01,1,0,0,0,600000,500,0.005);
+//globalEarth = new Planets('Earth',0.976,1.010,0.0000425,0.01,1,0,0,0,600000,500,0.005);
 
 
-button1 = new Button('Parte 1',font,'part = 1',50,50,0xfffff,true,0.3,-500,-200,-500);
+button1 = new Button('Parte 1',font,'part1()',50,50,0xfffff,true,0.3,-500,-200,-500);
 button2 = new Button('Parte 2',font,'part = 2',50,50,0xfffff,true,0.3,-425,-200,-500);
 
-render();
+//render();
+}
+
+function part1(){
+
+text1 = new Text('parte 1',font,50,30,0x00000,true,0.9,0,0,0);
+var obj = sceneSync.instantiate('createSyncedObject', initData, true);
+obj.add(text1.mesh);
+sim.scene.updateAllBehaviors();
 }
 
 
